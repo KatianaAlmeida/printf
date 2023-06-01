1b0001e774abafafaef63ebc5ba2f8814300a526
KatianaAlmeida <Katianaalmeida48@gmail.com>
2023-06-01 01:34:55 -0700

    My commit

---
+++ printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _printf - produces output according to a format.
+* @format: character string.
+* Return:  number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+}
